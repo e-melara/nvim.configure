a10e75c1fae1693ac756174465dbcf286a11613d
Edwin Melara <edwin.melara@omr.gob.sv>
2024-06-13 14:29:21 -0600

    Agregando gitsign

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	'wuelnerdotexe/vim-enfocado',
-  	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme enfocado]])
-	end,
+ 'wuelnerdotexe/vim-enfocado',
+ lazy = false,
+ priority = 1000,
+ config = function()
+  vim.cmd([[colorscheme enfocado]])
+ end,
 }
